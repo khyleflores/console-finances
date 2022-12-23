339ba912f83058a92ae5d957ce32fa6cf38ecced
khyleflores <khyleflores10@gmail.com>
2022-12-23 16:17:49 +0000

    Greatest Increase and Decrease in profit added in console

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var finances = [
 ['Jan-2010', 1000],
 ['Feb-2010', 2000],//1000
 ['Mar-2010', -500],//-2500
-['Apr-2010', 3500]//4000
+['Apr-2010', 3500],//4000
+['May-2010', 2000]//-1500
 ];
 
 //The average of the changes in Profit/Losses over the entire period.
@@ -21,46 +22,14 @@ var finances = [
 
 var totalMonths = 0;
 var netTotal = 0;
+var totalChange = 0;
 var averageChange = 0;
 var monthlyProfits = [];
-var totalChange = 0;
-var mostProfitable= 0;
-var mostProfitableMonthIndex = 0;
-var leastProfitable= 0;
+var mostProfit= 0;
+var leastProfit= 0;
+var mostProfitableMonth = "";
+var leastProfitableMonth = "";
 
-/*
-
-// The total number of months included in the dataset.
-totalMonths = finances.length;
-console.log("Total month is " + totalMonths);
-
-//The net total amount of Profit/Losses over the entire period. 
-for (var i = 0; i < finances.length; i++) {
-  netTotal += finances[i][1];
-}
-
-console.log("Net total is " + netTotal); 
-
-//Track total change in profits from month to month
-//First get all the difference of each months ie Febuary - January and put in array
-//finances.length-1 to avoid Null to be substracted to the last index
-for (var i = 0; i < finances.length-1; i++) {
-  monthlyProfits.push(finances[i+1][1] - finances[i][1]);
-}
-//Add all monthlyProfits to get the total change in profits
-for (var i = 0; i < monthlyProfits.length; i++) {
-  totalChange += monthlyProfits[i];
-}
-//Total/Number of months to get the average of the changes over the entire period
-averageChange = totalChange / totalMonths;
-console.log("Total Change in Profits is " + totalChange); 
-console.log("average of the changes in Profit/Losses over the entire period " + averageChange); 
-
-//Calculating greatest increase in profits using Math.max()
-mostProfitable = Math.max(...monthlyProfits);
-console.log("Greatest increase is :" + mostProfitable);
-
-*/
 
 // The total number of months included in the dataset.
 totalMonths = finances.length;
@@ -69,17 +38,19 @@ console.log("Total month is " + totalMonths);
 finances[0].push(0);
 for (var i = 0; i < finances.length-1; i++) {
   finances[i+1].push(finances[i+1][1] - finances[i][1]);
+  monthlyProfits.push(finances[i+1][1] - finances[i][1]);
 }
 
 //The net total amount of Profit/Losses over the entire period. 
 for (var i = 0; i < finances.length; i++) {
   netTotal += finances[i][1];
-  totalChange += finances[i][2];
-
-  //Calculating greatest increase in profits using Math.max()
-  mostProfitable = Math.max(finances[i][2]);
-  
+  totalChange += finances[i][2];  
 }
+//Calculating greatest increase in profits using Math.max()
+mostProfit = Math.max(...monthlyProfits);
+//Calculating greatest decrease in profits using Math.min()
+leastProfit = Math.min(...monthlyProfits);
+
 console.log("Net total is " + netTotal); 
 
 //Total/Number of months to get the average of the changes over the entire period
@@ -87,12 +58,21 @@ averageChange = totalChange / totalMonths;
 console.log("Total Change in Profits is " + totalChange); 
 console.log("average of the changes in Profit/Losses over the entire period " + averageChange);
 
-console.log(mostProfitable);
 
 for (var i = 0; i < finances.length; i++) {
   
-  if(finances[i][2] == mostProfitable){
-     console.log("Greatest Increase in Profits: " + finances[i][0] + " ($" + finances[i][2] + ")");
+  if(finances[i][2] == mostProfit){
+    mostProfitableMonth = finances[i][0];
+    mostProfit = finances[i][2]
+     //console.log("Greatest Increase in Profits: " + finances[i][0] + " ($" + finances[i][2] + ")");
+  }
+  else if(finances[i][2] == leastProfit){
+    leastProfitableMonth = finances[i][0];
+    leastProfit = finances[i][2];
+    //console.log("Greatest Decrease in Profits: " + finances[i][0] + " ($" + finances[i][2] + ")");
   }
 }
+
+console.log("Greatest Increase in Profits: " + mostProfitableMonth + " ($" + mostProfit + ")");
+console.log("Greatest Decrease in Profits: " + leastProfitableMonth + " ($" + leastProfit + ")");
 
