d5427cee7ab4a8ee299315ce84d111423374818e
khyleflores <khyleflores10@gmail.com>
2022-12-23 17:08:49 +0000

    Comments added and print resulting analysis in console

--- index.js
+++ index.js
@@ -33,46 +33,54 @@ var leastProfitableMonth = "";
 
 // The total number of months included in the dataset.
 totalMonths = finances.length;
-console.log("Total month is " + totalMonths);
 
+//Inserting a 0 on the first array of profit to be able to use Math function and avoid NAN result.
 finances[0].push(0);
+//Adding the details of total change in profits from month to month
 for (var i = 0; i < finances.length-1; i++) {
   finances[i+1].push(finances[i+1][1] - finances[i][1]);
+  //another one-dimensional array to easily calculate the greatest increase and decrease later
   monthlyProfits.push(finances[i+1][1] - finances[i][1]);
 }
 
 //The net total amount of Profit/Losses over the entire period. 
 for (var i = 0; i < finances.length; i++) {
+  //gets the sum of profits
   netTotal += finances[i][1];
+  //gets the sum of the newly added column of total change in profits from month to month
   totalChange += finances[i][2];  
 }
+
+//Total divided by Number of months to get the average of the changes over the entire period
+averageChange = totalChange / totalMonths;
+
 //Calculating greatest increase in profits using Math.max()
 mostProfit = Math.max(...monthlyProfits);
 //Calculating greatest decrease in profits using Math.min()
 leastProfit = Math.min(...monthlyProfits);
 
-console.log("Net total is " + netTotal); 
-
-//Total/Number of months to get the average of the changes over the entire period
-averageChange = totalChange / totalMonths;
-console.log("Total Change in Profits is " + totalChange); 
-console.log("average of the changes in Profit/Losses over the entire period " + averageChange);
-
-
+//created a loop to find the most profited month 
 for (var i = 0; i < finances.length; i++) {
-  
+  //if column 3 matches the mostProfit - save the detail to the variables
   if(finances[i][2] == mostProfit){
     mostProfitableMonth = finances[i][0];
+    //added mostProfit in here to make sure that we are getting the same details of a month in case there are duplicates
     mostProfit = finances[i][2]
-     //console.log("Greatest Increase in Profits: " + finances[i][0] + " ($" + finances[i][2] + ")");
   }
+  //if column 3 matches the leastProfit - save the detail to the variables
   else if(finances[i][2] == leastProfit){
     leastProfitableMonth = finances[i][0];
+    //added leastProfit in here to make sure that we are getting the same details of a month in case there are duplicates
     leastProfit = finances[i][2];
-    //console.log("Greatest Decrease in Profits: " + finances[i][0] + " ($" + finances[i][2] + ")");
   }
 }
 
-console.log("Greatest Increase in Profits: " + mostProfitableMonth + " ($" + mostProfit + ")");
-console.log("Greatest Decrease in Profits: " + leastProfitableMonth + " ($" + leastProfit + ")");
+//Print resulting analysis in browser
+console.log("Financial Analysis" + 
+"\n----------------------------" +
+"\nTotal months: " + totalMonths +
+"\nTotal: $" + netTotal +
+"\nAverage Change: $" + averageChange +
+"\nGreatest Increase in Profits: " + mostProfitableMonth + " ($" + mostProfit + ")" +
+"\nGreatest Decrease in Profits: " + leastProfitableMonth + " ($" + leastProfit + ")");
 
